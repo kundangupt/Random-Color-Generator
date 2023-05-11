80688e4337f89b274d7aae92d67d7257dc09273b
kundan gupta <kukundan1993@gmail.com>
2023-05-11 19:54:49 +0530

    doc: updated

--- Color.js
+++ Color.js
@@ -2,10 +2,17 @@ const GetColor = () => {
     const randomNum =  Math.floor(Math.random()*16777215);
     const randomCode = "#" +randomNum.toString(16);
     document.body.style.backgroundColor =randomCode;
+    document.getElementById("color-code"). innerText = randomCode;
+
+    document.clipboard.writeText(randomCode);
 }
 
-
+// event Call
 document.getElementById('btn').addEventListener(
     'click',
     GetColor
 )
+
+
+// intial Call
+GetColor();
